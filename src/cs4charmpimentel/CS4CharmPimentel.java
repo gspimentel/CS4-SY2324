60db7ef4937d1de36db71cc773ffe0c454c0d808
CHARM <CHARM@172.25.27.29>
2023-08-29 16:14:12 +0800

    first java project

---
+++ src/cs4charmpimentel/CS4CharmPimentel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4charmpimentel;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4CharmPimentel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String bio = "Subject 1";
+        String phys = "Subject 2";
+        String chem = "Subject 3";
+        System.out.println(bio + phys + chem);
+    }
+    
+}
